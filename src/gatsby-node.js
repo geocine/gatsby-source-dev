451e1a688dbd4da552db58d6320fc1af8d9d2610
Aivan Monceller <aivan@monceller.com>
2019-04-18 00:00:32 +0800

    feat(headers): add cache headers

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -16,7 +16,17 @@ exports.sourceNodes = async ({ actions }, { username }) => {
   });
 
   // eslint-disable-next-line no-await-in-loop
-  const res = await axios.get(`https://dev.to/api/articles?${params}`);
+  const res = await axios.get(`https://dev.to/api/articles?${params}`, {
+    headers: {
+      "cache-control": "no-cache",
+      pragma: "no-cache",
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+      "upgrade-insecure-requests": 1
+    }
+  });
 
   // eslint-disable-next-line no-await-in-loop
   const { data } = res;
